dcea9a123254b048b30bcaaff493c9d87c6ee562
yeshintamira <yeshintamirayolanda12@gmail.com>
2024-10-27 17:46:02 +0700

    tambah batikData

--- app/src/main/java/com/example/batikrek/data/batikData.kt
+++ app/src/main/java/com/example/batikrek/data/batikData.kt
@@ -19,7 +19,7 @@ data class Batik(
             Batik(
                 name = "Batik Mega Mendung",
                 motif = "Motif Mega Mendung",
-                suitableFor = "Pesta, Acara Keluarga",
+                suitableFor = "Acara Formal, Acara Keluarga",
                 imageRes = R.drawable.batik_mega_mendung
             ),
             Batik(
@@ -27,7 +27,109 @@ data class Batik(
                 motif = "Motif Kawung",
                 suitableFor = "Acara Formal, Casual",
                 imageRes = R.drawable.batik_kawung
-            )
+            ),
+            Batik(
+                name = "Batik Truntum",
+                motif = "Motif Truntum",
+                suitableFor = "Acara Keluarga, Pernikahan",
+                imageRes = R.drawable.batik_truntum
+            ),
+            Batik(
+                name = "Batik Jlamprang",
+                motif = "Motif Jlamprang",
+                suitableFor = "Pernikahan, Pesta",
+                imageRes = R.drawable.batik_jlamprang
+            ),
+            Batik(
+                name = "Batik Ceplok",
+                motif = "Motif Ceplok",
+                suitableFor = "Acara Formal, Pernikahan",
+                imageRes = R.drawable.batik_ceplok
+            ),
+            Batik(
+                name = "Batik Sekar Jagad",
+                motif = "Motif Seka Jagad",
+                suitableFor = "Acara Formal, Acara Keluarga",
+                imageRes = R.drawable.batik_sekarjagad
+            ),
+            Batik(
+                name = "Batik Lasem",
+                motif = "Motif Lasem",
+                suitableFor = "Acara Formal, Pernikahan",
+                imageRes = R.drawable.batik_lasem
+            ),
+             Batik(
+                name = "Batik Buketan",
+                motif = "Motif Buketan",
+                suitableFor = "Pernikahan, Pesta",
+                imageRes = R.drawable.batik_buketan
+            ),
+            Batik(
+                name = "Batik Pekalongan",
+                motif = "Motif Pekalongan",
+                suitableFor = "Acara Formal, Pernikahan",
+                imageRes = R.drawable.batik_pekalongan
+            ),
+             Batik(
+                name = "Batik Sidomukti",
+                motif = "Motif Sidomukti",
+                suitableFor = "Acara Formal, Pernikahan",
+                imageRes = R.drawable.batik_sidomukti
+            ),
+              Batik(
+                name = "Batik Cuwiri",
+                motif = "Motif Cuwiri",
+                suitableFor = "Acara Keluarga, Casual",
+                imageRes = R.drawable.batik_cuwiri
+            ),
+              Batik(
+                name = "Batik Bunga",
+                motif = "Motif Bunga",
+                suitableFor = "Pesta, Casual",
+                imageRes = R.drawable.batik_bunga
+            ),
+              Batik(
+                name = "Batik Tujuh Rupa",
+                motif = "Motif Tujuh Rupa",
+                suitableFor = "Acara Keluarga, Pesta",
+                imageRes = R.drawable.batik_tujuhrupa
+            ),
+               Batik(
+                name = "Batik Barong",
+                motif = "Motif Barong",
+                suitableFor = "Acara Formal, Pernikahan",
+                imageRes = R.drawable.batik_barong
+            ),
+               Batik(
+                name = "Batik Batu",
+                motif = "Motif Batu",
+                suitableFor = "Acara Formal, Pernikahan",
+                imageRes = R.drawable.batik_batu
+            ),
+               Batik(
+                name = "Batik Selendang",
+                motif = "Motif Selendang",
+                suitableFor = "Casual, Pesta",
+                imageRes = R.drawable.batik_selendang
+            ),
+              Batik(
+                name = "Batik Lurik",
+                motif = "Motif Lurik",
+                suitableFor = "Acara Keluarga, Casual",
+                imageRes = R.drawable.batik_lurik
+            ),
+              Batik(
+                name = "Batik Cempaka",
+                motif = "Motif Cempaka",
+                suitableFor = "Pernikahan, Pesta",
+                imageRes = R.drawable.batik_cempaka
+            ),
+              Batik(
+                name = "Batik Tulis",
+                motif = "Motif Tulis",
+                suitableFor = "Acara Formal, Pernikahan",
+                imageRes = R.drawable.batik_tulis
+            ),
         )
     }
 }
